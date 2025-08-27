1c30430120a848200f5d904a127e5bd51fab6049
Bharat Agarwal <bharatagarwal@gmail.com>
2025-08-27 14:57:14 +0530

    Chapter extraction of 6/10 books done

--- ingest.py
+++ ingest.py
@@ -1,3 +1,4 @@
+import os
 import re
 
 books = (
@@ -20,8 +21,12 @@ end = r"\*\*\* END OF THE PROJECT GUTENBERG EBOOK .*? \*\*\*"
 combined_regex = rf"{start}(.*?)({end})"
 
 for book in books:
-    with open(f"txt/{book}.txt", "r") as f:
+    with open(f"source/{book}.txt", "r") as f:
         raw_content = f.read()
 
         if content := re.search(combined_regex, raw_content, re.DOTALL):
-            pass
+            extracted_content = content.group(1).strip()
+            with open(f"books/{book}.py", "w") as out_f:
+                out_f.write('"""\n')
+                out_f.write(extracted_content)
+                out_f.write('\n"""')

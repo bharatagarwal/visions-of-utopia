e489a66931510821ff68c60745b9c94650e9d44a
Bharat Agarwal <bharatagarwal@gmail.com>
2025-08-31 12:17:34 +0530

    initial chunking effort

--- ingest.py
+++ ingest.py
@@ -1,32 +1,50 @@
-import os
-import re
+from books.a_crystal_age import chapter1, chapter2
+import spacy
 
-books = (
-    "a-crystal-age",
-    "a-modern-utopia",
-    "herland",
-    "little-fuzzy",
-    "looking-backward",
-    "new-atlantis",
-    "news-from-nowhere",
-    "the-last-evolution",
-    "the-story-of-utopias",
-    "utopia",
-)
+nlp = spacy.load("en_core_web_md")
+doc = nlp(chapter2)
 
+chunks = list()
+current_chunk_sentences = list()
+similarity_threshold = 0.88
+min_chunk_length = 250
+max_chunk_length = 500
 
-start = r"\*\*\* START OF THE PROJECT GUTENBERG EBOOK .*? \*\*\*"
-end = r"\*\*\* END OF THE PROJECT GUTENBERG EBOOK .*? \*\*\*"
+sentences = list(doc.sents)
 
-combined_regex = rf"{start}(.*?)({end})"
+for i, sentence in enumerate(sentences):
+    if not current_chunk_sentences:
+        current_chunk_sentences.append(sentence)
+        continue
 
-for book in books:
-    with open(f"source/{book}.txt", "r") as f:
-        raw_content = f.read()
+    current_chunk = " ".join(
+        [sent.text for sent in current_chunk_sentences]
+    )
+    current_chunk_doc = nlp(current_chunk)
+    similarity = sentence.similarity(current_chunk_doc)
+    print(f"{similarity}: {sentence}")
 
-        if content := re.search(combined_regex, raw_content, re.DOTALL):
-            extracted_content = content.group(1).strip()
-            with open(f"books/{book}.py", "w") as out_f:
-                out_f.write('"""\n')
-                out_f.write(extracted_content)
-                out_f.write('\n"""')
+    potential_chunk = current_chunk + " " + sentence.text
+
+    if (
+        similarity >= similarity_threshold
+        and len(potential_chunk) <= max_chunk_length
+    ):
+        current_chunk_sentences.append(sentence)
+    else:
+        if len(current_chunk) >= min_chunk_length:
+            chunks.append(current_chunk)
+        else:
+            current_chunk_sentences.append(sentence)
+            if len(potential_chunk) >= min_chunk_length:
+                chunks.append(potential_chunk)
+                current_chunk_sentences = []
+            continue
+        current_chunk_sentences = [sentence]
+
+if current_chunk_sentences:
+    final_chunk = " ".join(
+        [sent.text for sent in current_chunk_sentences]
+    )
+    if len(final_chunk) >= min_chunk_length:
+        chunks.append(final_chunk)
